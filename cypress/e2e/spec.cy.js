4a2434de24a49b8e16bdfc33f919dba24c9dbd73
Thomas-Smith0176 <thomas_smith@talktalk.net>
2024-02-22 14:47:08 +0000

    creates successful login test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,4 +12,13 @@ describe('Saucedemo login test', () => {
     cy.get('[data-test="username"]').should('have.value', 'standard_user')
     cy.get('[data-test="password"]').should('have.value', 'secret_sauce')
   })
+  it('Visits store page when given correct login info', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+
+    cy.url().should('include', 'inventory.html')
+  })
 })
