a79b837c1b3884d63f803a249b14a22755c38f09
Thomas-Smith0176 <thomas_smith@talktalk.net>
2024-02-22 14:39:39 +0000

    creates visit page and text field tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Saucedemo login test', () => {
+  it('Visits saucedemo.com', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.url().should('include', 'www.saucedemo.com')
+  })
+  it('Holds username and password in correct fields', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+
+    cy.get('[data-test="username"]').should('have.value', 'standard_user')
+    cy.get('[data-test="password"]').should('have.value', 'secret_sauce')
   })
 })
